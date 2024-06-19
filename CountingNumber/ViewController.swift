deb11e3616101f45c8c0bed2c95fcebd59ce679a
soobeen27 <youdew27@gmail.com>
2024-06-19 10:55:31 +0900

    Initial Commit

---
+++ CountingNumber/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountingNumber
+//
+//  Created by Soo Jang on 6/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
